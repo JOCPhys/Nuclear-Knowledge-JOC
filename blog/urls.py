5d18107409801f9c53b8f70b85d15c832d6f9139
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-10 16:25:09 +0000

    create URLS for authentication and views

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.landing_page, name='landing_page'),
+    path('topic/', views.topic_page, name='topic_page'),
+    path('blog/<int:pk>/', views.blog_page, name='blog_page'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
