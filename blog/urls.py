ffc075b60788e6a1625a9137e2c0164ee349ed98
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-10 16:35:30 +0000

    add templates for CRUD functionality creat_blog.html and create_blog.html

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('blog/<int:pk>/', views.blog_page, name='blog_page'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('create_blog/', views.create_blog, name='create_blog'),
+    path('blog/<int:pk>/create_comment/', views.create_comment, name='create_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog, Comment
+from .forms import BlogForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -16,3 +17,31 @@ def blog_page(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     comments = blog.comments.all()
     return render(request, 'blog_page.html', {'blog': blog, 'comments': comments})
+
+@login_required
+def create_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            return redirect('topic_page')
+    else:
+        form = BlogForm()
+    return render(request, 'create_blog.html', {'form': form})
+
+@login_required
+def create_comment(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.blog = blog
+            comment.save()
+            return redirect('blog_page', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'create_comment.html', {'form': form})
