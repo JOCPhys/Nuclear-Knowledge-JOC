59869e1f9b18f1dc3eeeb52f320fa0ba2b31c46e
Joanna O'Connor <jtoconnor75@gmail.com>
2025-01-14 17:06:50 +0000

    fix bug to enable admin to approve topic draft topic posts for publishing

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Topic, Comment
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django_summernote.admin import SummernoteModelAdmin
 from django.utils.html import format_html
+from .models import Topic, Comment
 
 @admin.register(Topic)
 class TopicAdmin(SummernoteModelAdmin):
+    class Media:
+        css = {
+            'all': (staticfiles_storage.url('css/admin.css'),)
+        }
 
     list_display = ('title', 'slug', 'excerpt', 'created_at', 'updated_at', 'published')
     search_fields = ['title']
@@ -12,6 +17,12 @@ class TopicAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
     date_hierarchy = 'created_at'
+    actions = ['approve_topics']
+
+    def approve_topics(self, request, queryset):
+        queryset.update(published=True)
+    approve_topics.short_description = "Approve selected topics"
+
 
     def changelist_view(self, request, extra_context=None):
         extra_context = extra_context or {}
